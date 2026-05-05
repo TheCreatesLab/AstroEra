178028959f1c3c6452415a18796258774e5d57e8
TheCreatesLab <thecreateslab@gmail.com>
2026-05-06 01:53:34 +0530

    Fix date rendering

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }
+const nextConfig = { 
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 's-maxage=86400, stale-while-revalidate' }
+        ]
+      }
+    ]
+  }
+}
 module.exports = nextConfig
